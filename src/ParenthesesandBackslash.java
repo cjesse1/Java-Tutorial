a5382fab8f2dab565f2463a9d7ca35683db9a55d
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-10-03 12:41:16 +0100

    .

---
+++ src/ParenthesesandBackslash.java
@@ -0,0 +1,10 @@
+public class ParenthesesandBackslash {
+    public static void main(String[]args){
+        //putting a word in parentheses
+        System.out.println("Hello \"jesse\"");
+        // c:\Windows\...
+        System.out.println("c:\\Windows\\...");
+        System.out.println("c:\t Windows\\...");
+        System.out.println("c:\nWindows\\");
+    }
+}
