ff0a8fc6d260eb95548682fd000993bba6e59c45
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-10-03 12:55:47 +0100

    .

---
+++ src/BasicMath.java
@@ -0,0 +1,16 @@
+public class BasicMath {
+    public static void main(String[]args){
+        byte j = 8;
+        byte k = 10;
+        System.out.println(j+" "+k);
+        System.out.println("addition: "j+k);
+        byte result = 10+3;
+        System.out.println(result);
+        byte result2 = 10-3;
+        System.out.println(result2);
+        byte result3 = 10*3;
+        System.out.println(result3);
+        float result4 = (float) 10/3;
+        System.out.println(result4);
+    }
+}
