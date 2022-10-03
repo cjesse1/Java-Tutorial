863e368fdba89245b4a6f5718813c1587b662d32
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-10-03 13:17:59 +0100

    .

---
+++ src/strings2.java
@@ -0,0 +1,9 @@
+public class strings2 {
+    public static void main(String[]args) {
+        String[] numbers = {"4","3"};
+        System.out.println(numbers.length);
+        System.out.println(numbers[0]);
+        String name = "Jesse";
+        System.out.println(name.length());
+    }
+}
