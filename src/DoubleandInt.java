a3e2b7703608d6909fb8b78c990adbceffce6adb
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-10-03 13:05:33 +0100

    .

---
+++ src/DoubleandInt.java
@@ -0,0 +1,16 @@
+public class DoubleandInt {
+    public static void main(String[]args){
+        int O = 10 + 3 * 2;
+        System.out.println(O);
+        int K = 1;
+        int J = K+2;
+        System.out.println(J);
+        double M = 1.6;
+        double N = M + 2.3;
+        System.out.println(N);
+        //adding double and int
+        double H = 1.1;
+        int I = (int)H + 2;
+        System.out.println(I);
+    }
+}
