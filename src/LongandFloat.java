3e28b62aba598507666dcd0253cb88b3cc39fe16
Chiakwelu Jesse <chiakwelujesse@gmail.com>
2022-10-03 13:22:59 +0100

    .

---
+++ src/LongandFloat.java
@@ -0,0 +1,8 @@
+public class LongandFloat {
+    public static void main(String[]args){
+        long viewsCount = 4_123_456_789L;
+        float price = 10.99F;
+        System.out.println(price);
+        System.out.println(viewsCount);
+    }
+}
